9d4d8bed9f8a119d26244497f437134d6f8bc8b8
chrisnastro <cnastro@icloud.com>
2023-11-29 21:22:59 -0500

    fetched all relevant restaurant info based on search city

--- restaurant.js
+++ restaurant.js
@@ -2,15 +2,6 @@ var userLocationInput = document.getElementById("location-input");
 var userFoodType = document.getElementById("foodtype-input");
 var searchButton = document.getElementById("search-button");
 
-
-
-
-
-function renderLocation() {
-	var location = localStorage.getItem("location-input");
-
-
-var locationUrl = 'https://tripadvisor16.p.rapidapi.com/api/v1/restaurant/searchLocation?query=' + location;
 const locationOptions = {
 	method: 'GET',
 	headers: {
@@ -18,24 +9,7 @@ const locationOptions = {
 		'X-RapidAPI-Host': 'tripadvisor16.p.rapidapi.com'
 	}
 };
-console.log(location);
-console.log(locationUrl);
 
-fetch(locationUrl, locationOptions)
-.then(function (response) {
-	return response.json();
-})
-.then(function (data) {
-	for (var i = 0; i < 1; i++) {
-		console.log(data.data[i].locationId);
-	}
-});
-fetch (locationUrl, locationOptions).then((response) => response.json()).then((data) => console.log(data));
-};
-
-
-
-const searchUrl = 'https://tripadvisor16.p.rapidapi.com/api/v1/restaurant/searchRestaurants?locationId=60763';
 const searchOptions = {
 	method: 'GET',
 	headers: {
@@ -44,8 +18,32 @@ const searchOptions = {
 	}
 };
 
-const url = 'https://tripadvisor16.p.rapidapi.com/api/v1/restaurant/getRestaurantDetails?restaurantsId=Restaurant_Review-g60763-d11918545-Reviews-Boucherie_West_Village-New_York_City_New_York';
-const options = {
+function getLocationUrl(location) {
+return 'https://tripadvisor16.p.rapidapi.com/api/v1/restaurant/searchLocation?query=' + location;
+}
+
+function getSearchUrl(locationId) {
+	return 'https://tripadvisor16.p.rapidapi.com/api/v1/restaurant/searchRestaurants?locationId=' + locationId;
+}
+
+function renderLocation(location) {
+
+fetch(getLocationUrl(location), locationOptions)
+.then(function (response) {
+	return response.json();
+})
+.then(function (data) {
+	 return fetch(getSearchUrl(data.data[0].locationId), searchOptions)
+})
+.then(function (response) {
+	return response.json();
+})
+.then(function(data){console.log(data)}) 
+};
+
+
+const restaurantUrl = 'https://tripadvisor16.p.rapidapi.com/api/v1/restaurant/getRestaurantDetails?restaurantsId=Restaurant_Review-g60763-d11918545-Reviews-Boucherie_West_Village-New_York_City_New_York';
+const restaurantOptions = {
 	method: 'GET',
 	headers: {
 		'X-RapidAPI-Key': '261cf2e304msh1aa969bcf7b92f1p18970ejsna94988a201e7',
@@ -61,9 +59,9 @@ searchButton.addEventListener("click", function(event) {
 
 	localStorage.setItem("location-input", location);
 	localStorage.setItem("foodtype-input", foodType);
-	renderLocation();
+	renderLocation(location);
 })
 
-fetch (searchUrl, searchOptions).then((response) => response.json()).then((data) => console.log(data));
-fetch (url, options).then((response) => response.json()).then((data) => console.log(data));
+// fetch (searchUrl, searchOptions).then((response) => response.json()).then((data) => console.log(data));
+// fetch (url, options).then((response) => response.json()).then((data) => console.log(data));
 // fetch (locationUrl, locationOptions).then((response) => response.json()).then((data) => console.log(data));
