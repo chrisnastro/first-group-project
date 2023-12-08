e079e9372f5ff3c793fc3417aa27d0f7568e07f8
juna-2017 <juna.okon@yahoo.com>
2023-12-08 13:34:09 -0500

    changed format of results to show like a grid and added function to move positioning for search form once results are loaded

--- script.js
+++ script.js
@@ -1,16 +1,2 @@
-// var resultsContainerTwo = document.getElementById("results-containerTwo");
-var mainResults = document.getElementById("main-results");
-
-// const restOptions = {
-// 	method: 'GET',
-// 	headers: {
-// 		'X-RapidAPI-Key': '261cf2e304msh1aa969bcf7b92f1p18970ejsna94988a201e7',
-// 		'X-RapidAPI-Host': 'tripadvisor16.p.rapidapi.com'
-// 	}
-// };
-
-// function getRestaurantUrl(restaurantsId) {
-// 	return 'https://tripadvisor16.p.rapidapi.com/api/v1/restaurant/getRestaurantDetails?restaurantsId=' + restaurantsId;
-// }
 
 
